ead85d179ec57ab7783f6582df4f2aef00e96da8
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2021-09-30 19:02:00 -0500

    stacked lines or scatter option // average or sum

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import requests
 import json
@@ -50,6 +51,13 @@ app.layout = html.Div(children=[
         value='voyage_itinerary__imp_port_voyage_begin',
         multi=False
     ),
+    html.Label('AVERAGE, SUM // OR SHOW INDIVIDUAL DATAPOINTS'),
+    dcc.RadioItems(
+                id='group_mode',
+                options=[{'label': i, 'value': i} for i in ['AVERAGE BY FACTOR', 'SUM BY FACTOR','INDIVIDUAL DATAPOINTS']],
+                value='SUM BY FACTOR',
+                labelStyle={'display': 'inline-block'}
+            ),
     	html.Label('Years'),
     dcc.RangeSlider(
         id='year-slider',
@@ -74,22 +82,55 @@ def update_df(yr):
 
 @app.callback(
 	Output('voyages-scatter-graph', 'figure'),
+	Input('group_mode', 'value'),
 	Input('x_vars', 'value'),
 	Input('y_vars', 'value'),
 	Input('factors', 'value'),
 	Input('memory','data')
 	)
-def update_figure(x_val,y_val,color_val,j):
+
+def update_figure(group_mode,x_val,y_val,color_val,j):
 	#filtered_df = df[df.year == selected_year]
 	df=pd.read_json(j)
-	df=df.fillna(0)
-	fig = px.scatter(df,
+	colors=df[color_val].unique()	
+	
+	if group_mode != 'INDIVIDUAL DATAPOINTS':
+		fig=go.Figure()
+		for color in colors:
+			df2=df.loc[df[color_val]==color]
+			
+			if group_mode=='AVERAGE BY FACTOR':
+				df2=df2.groupby(x_val)[y_val].mean()
+			elif group_mode=='SUM BY FACTOR':
+				df2=df2.groupby(x_val)[y_val].sum()
+		
+			x_vals=[i for i in df2.index]
+			y_vals=[df2[i] for i in x_vals]
+			trace_name=color
+		
+			fig.add_trace(go.Scatter(
+				x=x_vals,
+				y=y_vals,
+				name=trace_name,
+				stackgroup='one',
+				mode='none')
+			)
+		fig.update_layout(
+		xaxis_title=md[x_val]['label'],
+		yaxis_title=md[y_val]['label'],
+		transition_duration=200)
+
+	else:
+		df=df.fillna(0)
+		fig = px.scatter(df,
 		x=x_val,
 		y=y_val,
 		labels={x_val:md[x_val]['label'],y_val:md[y_val]['label'],color_val:md[color_val]['label']},
 		color=color_val
-	)
-	fig.update_layout(transition_duration=500)
+		)
+		fig.update_layout(transition_duration=500)
+
+	
 	return fig
 
 
