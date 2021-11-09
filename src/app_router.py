cbb770df5aa2a66dc33e4c2c037246bed7810e83
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2021-11-09 06:00:28 -0600

    merge w jcm10 recent changes

--- src/app_router.py
+++ src/app_router.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from app import app
-from apps import scatter_app,scatter_app_nomemory,sunburst_app,sunburst_app_nomemory
+from apps import scatter_app_nomemory,sunburst_app_nomemory
 
 #FROM https://dash.plotly.com/urls#structuring-a-multi-page-app
 
@@ -15,12 +15,8 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
 def display_page(pathname):
-    if pathname == '/apps/scatter_app':
-        return scatter_app.layout
-    elif pathname == '/apps/scatter_app_nomemory':
+    if pathname == '/apps/scatter_app_nomemory':
         return scatter_app_nomemory.layout
-    elif pathname == '/apps/sunburst_app':
-        return sunburst_app.layout
     elif pathname == '/apps/sunburst_app_nomemory':
         return sunburst_app_nomemory.layout
     else:
